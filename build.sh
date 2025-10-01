cd8d4901c6d6b52852d0fe290981ca9b2858b3e7
jayxunan <asunanadrianjay@gmail.com>
2025-10-01 10:27:51 +0800

    ewanqnlng

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ pip install -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-python - <<END
+# Safely create superuser only if database is ready
+python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='alignedadmin').exists():
     User.objects.create_superuser('alignedadmin', 'gyulfreak@gmail.com', 'y23adr4amnw0')
-END
+"
