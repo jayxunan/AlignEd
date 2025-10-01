76e00f80f08e494343df4557209570644e0cda01
jayxunan <asunanadrianjay@gmail.com>
2025-10-01 10:19:59 +0800

    na naman

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-# Exit if any command fails
-set -o errexit  
+set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('alignedadmin', 'gyulfreak@gmail.com', 'y23adr4amnw0')" | python manage.py shell
+
+python - <<END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='alignedadmin').exists():
+    User.objects.create_superuser('alignedadmin', 'gyulfreak@gmail.com', 'y23adr4amnw0')
+END
