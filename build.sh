e0db05ad0eba2a352e9abdf5f29fbb030d274fa9
jayxunan <asunanadrianjay@gmail.com>
2025-10-01 09:23:57 +0800

    Add build script for Render

--- build.sh
+++ build.sh
@@ -2,11 +2,7 @@
 # Exit if any command fails
 set -o errexit  
 
-# Install dependencies (Render already does pip install -r requirements.txt, but this is safe)
 pip install -r requirements.txt
 
-# Run database migrations
 python manage.py migrate --noinput
-
-# Collect static files
 python manage.py collectstatic --noinput
