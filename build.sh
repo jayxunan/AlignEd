7c9ecc50fff3916f17333557725b05906410fe30
jayxunan <asunanadrianjay@gmail.com>
2025-10-01 10:12:02 +0800

    soper yoser

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('alignedadmin', 'gyulfreak@gmail.com', 'y23adr4amnw0')" | python manage.py shell
