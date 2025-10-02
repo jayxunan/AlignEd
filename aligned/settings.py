9e422df7387c13a20fb69e045442f1b73d621aee
jayxunan <asunanadrianjay@gmail.com>
2025-10-02 08:57:14 +0800

    setings

--- aligned/settings.py
+++ aligned/settings.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# secret key
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-dev-key-for-aligned")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
@@ -26,7 +24,6 @@ if DEBUG:
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# define apps
 INSTALLED_APPS = [
     'recommender',
     'django.contrib.admin',
@@ -92,6 +89,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# 2fa
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
@@ -99,6 +97,7 @@ EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL")
+
 
 LOGIN_URL = 'login'
 CACHES = {'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}}
