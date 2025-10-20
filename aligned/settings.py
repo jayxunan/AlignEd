97ef2126a42cb8a21f7267dd4e1b1d06c1d9dd1d
jayxunan <asunanadrianjay@gmail.com>
2025-10-21 03:46:45 +0800

    sanaasdfasdf

--- aligned/settings.py
+++ aligned/settings.py
@@ -105,6 +105,7 @@ EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL")
+EMAIL_TIMEOUT = 10
 
 LOGIN_URL = 'login'
 CACHES = {'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}}
